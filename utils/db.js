48cf1a094209d7f69db5f042edc9031a280963b6
Garima Thakore <gthakore@horizontal.com>
2024-07-05 15:55:08 +0530

    set true

--- utils/db.js
+++ utils/db.js
@@ -13,6 +13,7 @@ async function connect() {
     }
     await mongoose.disconnect();
   }
+  mongoose.set('strictQuery', true);
   const db = await mongoose.connect(process.env.MONGODB_URI);
   connection.isConnected = db.connections[0].readyState;
 }
