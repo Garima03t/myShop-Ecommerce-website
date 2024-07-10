280e8b5dd599c13fd5cde020c49ca2852f49e27d
Garima Thakore <gthakore@horizontal.com>
2024-07-10 08:53:46 +0530

    login changes

--- pages/api/orders/[id]/index.js
+++ pages/api/orders/[id]/index.js
@@ -4,9 +4,9 @@ import { getSession } from "next-auth/react";
 
 const handler = async (req, res) => {
   const session = await getSession({ req });
-  if (!session) {
-    res.status(401).send("Signin required");
-  }
+  // if (!session) {
+  //   res.status(401).send("Signin required");
+  // }
   await db.connect();
   const order = await Order.findById(req.query.id);
   await db.disconnect();
